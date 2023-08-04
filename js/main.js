15e676e9eb280bc90dd1846b85a1b5bd55ed1141
timotherenard <timo.renard@hotmail.fr>
2023-08-04 22:28:33 +0400

    try

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+function menuon(){
+	document.getElementsByClassName("headlink")[0].className="responsivemenu";
+}
+
+function menuoff(){
+	document.getElementsByClassName("responsivemenu")[0].className="headlink";
+}
